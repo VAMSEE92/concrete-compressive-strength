ce2fa277f2575a3e9a13020342d64eaa6bcec64d
Vamsee Krishna <42322588+VAMSEE92@users.noreply.github.com>
2022-08-22 12:34:01 +0530

    Deleted extra directory

--- app/client.py
+++
@@ -1,6 +0,0 @@
-import requests
-import json
-url="http://ec2-65-0-173-133.ap-south-1.compute.amazonaws.com:8080/api"
-data=json.dumps({'cement':15,'slag':4,'ash':35,'water':91,'superplastic':8,'age':7})
-r=requests.post(url,data)
-print(r.json())
